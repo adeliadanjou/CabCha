9141043344290094c6d80d437daa15a8f371891c
Adelia d Anjou <princessadelia@gmail.com>
2018-12-26 20:31:19 +0100

    get & post of restaurants. I can create restaurants and see all of them. Also created the delete which deletes both all eaters and restaurants

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 const Eater = require('../models/eater');
+const Restaurant = require('../models/restaurant');
 
 router.get('/', (req, res, next) => {
   res.render('index');
 });
-
-// EATERS - GET
-router.get('/eaters', (req, res, next) => {
-	Eater.find({},(error, eatersFromDB) => {
-		if (error) { 
-			next(error); 
-		} else { 
-			res.send(200, eatersFromDB);
-		}
-	});
-});
-
 
 //EATERS - POST 
 
@@ -36,7 +25,66 @@ router.post('/eaters', (req, res, next) => {
 	});
 });
 
-//CREATING RESTAURANTS - POST
+// EATERS - GET
+router.get('/eaters', (req, res, next) => {
+	Eater.find({},(error, eatersFromDB) => {
+		if (error) { 
+			next(error); 
+		} else { 
+			res.send(200, eatersFromDB);
+		}
+	});
+});
+
+// RESTAURANTS - POST
+
+router.post('/restaurants', (req, res, next) => {
+
+	const newRestaurant = new Restaurant({
+		name: req.body.name,
+		address: req.body.address
+	});
+
+	newRestaurant.save((error) => {
+		if (error) { 
+			next(error); 
+		} else { 
+			res.send({message: "created"});
+		}
+	});
+});
+
+// RESTAURANTS - GET
+
+router.get('/restaurants', (req, res, next) => {
+	Restaurant.find({},(error, restaurantFromDB) => {
+		if (error) { 
+			next(error); 
+		} else { 
+			res.send(200, restaurantFromDB);
+		}
+	});
+});
+
+// EATERS - DELETE (Deleting all eaters and restaurants)
+
+router.delete('/eaters',(req, res, next) => {
+	
+	Restaurant.remove({}, function(error, restaurant) {
+		if (error) {
+			next(error);
+		} else {
+			res.send({message: "eaters and restaurants removed"});
+		}
+	});
+	Eater.remove({}, function(error, eater) {
+		if (error) {
+			next(error);
+		} else {
+			res.send({message: "eaters and restaurants removed"});
+		}
+	});
+});
 
 
 module.exports = router;
